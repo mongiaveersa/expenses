02e6171c925259b41551dacfd3c4e6142341b143
Shubham Mongia <shubham@veersatech.com>
2021-08-13 12:47:41 +0530

    Gemfile changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :description, presence: true, length: {minimum: 10, maximum:200}
-    validates :amount, presence: true
+    validates :amount, presence: true, numericality: {greater_than: 0}
 end
